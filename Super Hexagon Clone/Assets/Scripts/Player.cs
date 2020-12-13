c45c0dd869be441e172fc22973ab05fbdae47333
MeanBachine <david.flyr@gmail.com>
2020-12-13 01:05:09 -0600

    Add player movement
    Created Player script to control rotation

--- Super Hexagon Clone/Assets/Scripts/Player.cs
+++ Super Hexagon Clone/Assets/Scripts/Player.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float _moveSpeed = 600f;
+
+    float _movement = 0f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        _movement = Input.GetAxisRaw("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.forward, _movement * Time.fixedDeltaTime * -_moveSpeed);
     }
 }
